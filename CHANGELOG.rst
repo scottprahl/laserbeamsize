c4377bcd97747142d871fb6e1363ae74d5c585b7
Scott Prahl <scott@prahls.com>
2020-03-29 09:43:15 -0700

    using static cfg

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,18 +1,28 @@
-v 1.0.1
-      trivial fix for release.txt
-      improve text
-      remove completed tasks
-      initial commit of 07-M2-Measurement.ipynb
-      bump version
+Changelog
+=================================================
+
+v1.02
+------
+*    use sphinx for documentation
+
+v1.0.1
+------
+*    trivial fix for release.txt
+*    improve text
+*    remove completed tasks
+*    initial commit of 07-M2-Measurement.ipynb
+*    bump version
 
 v1.0.0
-      first pass at docs
-      General doc improvements
-      add routine to draw default figure
-      handle symmetric case dx=dy better
-      add new definitions and test dx=dy case
+------
+*    first pass at docs
+*    General doc improvements
+*    add routine to draw default figure
+*    handle symmetric case dx=dy better
+*    add new definitions and test dx=dy case
 
 v0.2.0
-      initial commit
-      ensure float used for sums
-      first public release
+------
+*    initial commit
+*    ensure float used for sums
+*    first public release
--- setup.py
+++ setup.py
@@ -1,37 +1,18 @@
-"""
-Copyright 2017 Scott Prahl
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-"""
-
 from setuptools import setup
 
+# long_description =
+#     Simple and fast calculation of beam sizes from a single monochrome image based
+#     on the ISO 11146 method of variances.  Some effort has been made to make
+#     the algorithm less sensitive to background offset and noise.
+
+# use README.rst as the long description
+# make sure to use the syntax that works in both ReST and markdown
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-	name='laserbeamsize',
-	packages=['laserbeamsize'],
-	version='1.0.1',
-	description='ISO 11146 Calculation of Laser Beam Center and Diameter',
-	url='https://github.com/scottprahl/laserbeamsize.git',  
-	author='Scott Prahl',
-	author_email='scott.prahl@oit.edu',
-	license='MIT',
-	classifiers=[
-		'Development Status :: 4 - Beta',
-		'License :: OSI Approved :: MIT License',
-		'Intended Audience :: Science/Research',
-		'Programming Language :: Python',
-		'Topic :: Scientific/Engineering :: Physics',
-	],
-	keywords=['variance', 'gaussian', 'M-squared', 'd4sigma'],
-	install_requires=['numpy','matplotlib'],
-	long_description=
-	"""
-	Simple and fast calculation of beam sizes from a single monochrome image based
-	on the ISO 11146 method of variances.  Some effort has been made to make
-	the algorithm less sensitive to background offset and noise.
-	""",
+    long_description=long_description,
+    long_description_content_type='text/x-rst'
 )
