7e95aae769ca02ca6316b33bcc185d15ddd2bdcd
Scott Prahl <scott@prahls.com>
2020-03-29 11:39:06 -0700

    move description to source

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup
 
-# long_description =
-#     Simple and fast calculation of beam sizes from a single monochrome image based
-#     on the ISO 11146 method of variances.  Some effort has been made to make
-#     the algorithm less sensitive to background offset and noise.
-
 # use README.rst as the long description
 # make sure to use the syntax that works in both ReST and markdown
 from os import path
