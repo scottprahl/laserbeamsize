fd7815aab8967626b7d13f27db28dfb40a1d1bfd
Scott Prahl <scott@prahls.com>
2022-03-11 10:41:35 -0800

    remove unused

--- docs/conf.py
+++ docs/conf.py
@@ -44,9 +44,6 @@ numpydoc_show_class_members = False
 napoleon_use_param = False
 napoleon_use_rtype = False
 
-# Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
