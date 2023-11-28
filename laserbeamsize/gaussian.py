fix focused beam size calculation

laserbeamsize/gaussian.py
@@ -252,7 +252,7 @@ def focused_diameter(f, lambda0, d, M2=1):
     Returns:
         d: diffraction-limited beam diameter [m]
     """
-    return 4 * M2**2 * lambda0 * f / (np.pi * d)
+    return 4 * M2 * lambda0 * f / (np.pi * d)
 
 
 def beam_parameter_product(Theta, d0, Theta_std=0, d0_std=0):

tests/test_gaussian.py
@@ -215,9 +215,9 @@ def test_focused_diameter_varied_aperture_diameter():
 
 
 def test_focused_diameter_with_m2_factor():
-    f, lambda0, d, M2 = 0.01, 1e-6, 0.002, 2
+    f, lambda0, d, M2 = 120e-3, 532e-9, 10e-3, 1.3
     result = lbs.focused_diameter(f, lambda0, d, M2)
-    assert np.isclose(result, 2.5464791e-05)  # replace with the correct 8-digit accurate result
+    assert np.isclose(result, 0.010567e-03)  # replace with the correct 8-digit accurate result
 
 
 # beam parameter product